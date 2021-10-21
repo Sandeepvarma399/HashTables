7e7f7f0a3dafa2e0d1567f0b364c9585ec6e6248
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-21 13:08:25 +0530

    Added:remove words

--- src/main/java/org/blz/hashmap/MyLinkedHashMap.java
+++ src/main/java/org/blz/hashmap/MyLinkedHashMap.java
@@ -8,7 +8,7 @@ public class MyLinkedHashMap<K,V> {
     private final int numBuckets;
     ArrayList<LinkedList<K>> myBucketArray;
 
-    public <String, Integer> MyLinkedHashMap() {
+    public MyLinkedHashMap() {
         this.numBuckets = 10;
         this.myBucketArray = new ArrayList<LinkedList<K>>(numBuckets);
 
@@ -51,8 +51,25 @@ public class MyLinkedHashMap<K,V> {
         }
     }
 
+    public MyMapNode<K,V> remove(K key) {
+
+        int index = this.getBucketIndex(key);
+        LinkedList<K> myLinkedList = this.myBucketArray.get(index);
+        if(myLinkedList == null) {
+            System.out.println("No Linked List found.");
+            return null;
+        }
+        MyMapNode<K,V> tempNode = (MyMapNode<K,V>) myLinkedList.deleteNodeWithKey(key);
+        if(tempNode == null) {
+            return null;
+        }
+        else
+            return tempNode;
+    }
+
     @Override
     public String toString() {
         return "MyLinkedHashMap List{" + myBucketArray + '}';
     }
-}}
+
+}
